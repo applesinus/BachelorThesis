eb3bd17c0352eaa2434c8315beee1f852af5286f
applesinus <dmitryapelsin4ik@gmail.com>
2025-04-28 19:22:49 +0300

    constants starter

---
+++ engine/constants/constants.go
@@ -0,0 +1,22 @@
+package constants
+
+const (
+	// Window
+	WindowWidth  = 1080
+	WindowHeight = 720
+)
+
+const (
+	// Algorithm names
+	NoAlgo = "No Algorithm !TODO!"
+
+	BVH = "Bounding Volume Hierarchy"
+	SaP = "Sweep and Prune"
+	GJK = "Gilbert-Johnson-Keerthi"
+	SAT = "Separating Axis Theorem"
+	LCP = "Linear Complementarity Problem"
+	PGS = "Projected Gauss-Seidel"
+	TGS = "Temporal Gauss-Seidel"
+	MSI = "Method of sequential impulses"
+	EPA = "Expanding Polytope Algorithm"
+)
