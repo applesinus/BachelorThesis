703c5e37228bea6b69586bada65ae4f337321a83
applesinus <dmitryapelsin4ik@gmail.com>
2025-04-29 01:20:00 +0300

    add obj pool placeholder

--- engine/engine.go
+++ engine/engine.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"BachelorThesis/engine/objects"
 	"BachelorThesis/engine/visualizer"
 	"log"
 )
@@ -8,7 +9,11 @@ import (
 func Run(algorithm string, endChan chan bool) {
 	log.Printf("Simulation of %s started", algorithm)
 
-	visualizer.Start(algorithm, endChan)
+	pool := make([]objects.Object, 0)
+	obj1 := objects.NewSphere(1.0, "sphere1")
+	pool = append(pool, &obj1)
+
+	visualizer.Start(algorithm, endChan, pool)
 
 	log.Printf("Simulation of %s ended", algorithm)
 }
