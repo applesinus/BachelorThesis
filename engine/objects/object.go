c1457119c430d2f83c719ab7f68e980bb7772bcd
applesinus <dmitryapelsin4ik@gmail.com>
2025-04-28 19:23:22 +0300

    object & AABB interfaces

---
+++ engine/objects/object.go
@@ -0,0 +1,27 @@
+package objects
+
+import (
+	"BachelorThesis/engine/vector"
+)
+
+type BoundingBox struct {
+	Min *vector.Vector3D
+	Max *vector.Vector3D
+}
+
+type Object interface {
+	Update()
+	GetBoundingBox() (*BoundingBox, error)
+
+	SetPosition(vector.Vector3D)
+	GetPosition() (*vector.Vector3D, error)
+
+	AppllyVelocity(vector.Vector3D) error
+	GetVelocity() (*vector.Vector3D, error)
+
+	SetAngle(vector.Angle3D)
+	GetAngle() (*vector.Angle3D, error)
+
+	ApplyRotation(vector.Angle3D) error
+	GetRotation() (*vector.Angle3D, error)
+}
