1de88ce2e4546be071e4e5033381fbd95a32ca87
applesinus <dmitryapelsin4ik@gmail.com>
2025-04-29 14:14:55 +0300

    visualizer upd

--- engine/visualizer/visualizer.go
+++ engine/visualizer/visualizer.go
@@ -1,9 +1,8 @@
 package visualizer
 
 import (
+	"BachelorThesis/engine/constants"
 	"BachelorThesis/engine/objects"
-	"log"
-	"math"
 	"runtime"
 
 	hg "github.com/harfang3d/harfang-go"
@@ -14,14 +13,8 @@ const (
 )
 
 func Start(algorithm string, endChan chan bool, pool []objects.Object) {
-	hg.InputInit()
-	hg.WindowSystemInit()
 
-	//подготовка окна для рисования
-	var resX int32 = 1280
-	var resY int32 = 720
-	win := hg.RenderInitWithWindowTitleWidthHeightResetFlags("Harfang Sample", resX, resY, hg.RFMSAA4X)
-	pipeline := hg.CreateForwardPipelineWithShadowMapResolution(4096)
+	win, pipeline, scene := prepareScene()
 
 	//создание конвейера отрисовки
 	res := hg.NewPipelineResources()
@@ -33,15 +26,6 @@ func Start(algorithm string, endChan chan bool, pool []objects.Object) {
 	shader := hg.LoadPipelineProgramRefFromFile("resources_compiled/core/shader/default.hps", res, hg.GetForwardPipelineInfo())
 	//определение материала (для цвета задается RGB от 0 до 1)
 	sphereMat := hg.CreateMaterialWithValueName0Value0ValueName1Value1(shader, "uDiffuseColor", hg.NewVec4WithXYZ(1, 0, 0), "uSpecularColor", hg.NewVec4WithXYZ(1, 0.8, 0))
-
-	//создания.и конфигурация фонового цвета
-	scene := hg.NewScene()
-	scene.GetCanvas().SetColor(hg.NewColorWithRGB(0.1, 0.1, 0.1))
-	scene.GetEnvironment().SetAmbient(hg.NewColorWithRGB(0.1, 0.1, 0.1))
-
-	//установка камеры
-	cam := hg.CreateCamera(scene, hg.TransformationMat4(hg.NewVec3WithXYZ(15.5, 5, -6), hg.NewVec3WithXYZ(0.4, -1.2, 0)), 0.01, 100)
-	scene.SetCurrentCamera(cam)
 
 	//добавление света
 	hg.CreateSpotLightWithDiffuseDiffuseIntensitySpecularSpecularIntensityPriorityShadowTypeShadowBias(scene, hg.TransformationMat4(hg.NewVec3WithXYZ(-8.8, 21.7, -8.8), hg.Deg3(60, 45, 0)), 0, hg.Deg(5), hg.Deg(30), hg.ColorGetWhite(), 1, hg.ColorGetWhite(), 1, 0, hg.LSTMap, 0.000005)
@@ -56,31 +40,31 @@ func Start(algorithm string, endChan chan bool, pool []objects.Object) {
 		}
 		rows = append(rows, row)
 	}
-	angle := 0.0
-	rect := hg.NewIntRectWithSxSyExEy(0, 0, resX, resY)
-
-	log.Printf("Visualization objects created")
+	// angle := 0.0
+	rect := hg.NewIntRectWithSxSyExEy(0, 0, constants.WindowWidth, constants.WindowHeight)
 
 	//основной цикл
 	for !hg.ReadKeyboard().Key(hg.KEscape) && hg.IsWindowOpen(win) {
+		render(pool)
+
 		dt := hg.TickClock()
-		angle += float64(hg.TimeToSecF(dt))
+		/*angle += float64(hg.TimeToSecF(dt))
 
 		//поворот объектов сцены (узлов) на угол
 		for j, row := range rows {
 			rowY := math.Cos(angle + float64(j)*0.1)
 			for i, trs := range row {
-				/*pos := trs.GetPos()
-				pos.SetY(float32(0.1 * (rowY*math.Sin(angle+float64(i)*0.1)*6 + 6.5)))
-				trs.SetPos(pos)
-				*/
+				// pos := trs.GetPos()
+				// pos.SetY(float32(0.1 * (rowY*math.Sin(angle+float64(i)*0.1)*6 + 6.5)))
+				// trs.SetPos(pos)
+
 				p := hg.NewVec3()
 				p, _ = trs.GetPosRot()
 				p.SetY(float32(0.1 * (rowY*math.Sin(angle+float64(i)*0.1)*6 + 6.5)))
 				trs.SetPos(p)
 
 			}
-		}
+		}*/
 
 		//обновление сцены
 		scene.Update(dt)
@@ -104,4 +88,28 @@ func Start(algorithm string, endChan chan bool, pool []objects.Object) {
 
 	hg.RenderShutdown()
 	hg.DestroyWindow(win)
+}
+
+func render(pool []objects.Object) *hg.Transform {
+	return nil
+}
+
+func prepareScene() (*hg.Window, *hg.ForwardPipeline, *hg.Scene) {
+	hg.InputInit()
+	hg.WindowSystemInit()
+
+	//подготовка окна для рисования
+	win := hg.RenderInitWithWindowTitleWidthHeightResetFlags(title, constants.WindowWidth, constants.WindowHeight, hg.RFMSAA4X)
+	pipeline := hg.CreateForwardPipelineWithShadowMapResolution(4096)
+
+	//создания.и конфигурация фонового цвета
+	scene := hg.NewScene()
+	scene.GetCanvas().SetColor(hg.NewColorWithRGB(0.1, 0.1, 0.1))
+	scene.GetEnvironment().SetAmbient(hg.NewColorWithRGB(0.1, 0.1, 0.1))
+
+	//установка камеры
+	cam := hg.CreateCamera(scene, hg.TransformationMat4(hg.NewVec3WithXYZ(15.5, 5, -6), hg.NewVec3WithXYZ(0.4, -1.2, 0)), 0.01, 100)
+	scene.SetCurrentCamera(cam)
+
+	return win, pipeline, scene
 }
