ea0871a2a3b2bc22f6302ccf7dcf645d1399c812
applesinus <dmitryapelsin4ik@gmail.com>
2025-04-29 13:23:38 +0300

    HARFANG migration

--- engine/visualizer/visualizer.go
+++ engine/visualizer/visualizer.go
@@ -2,15 +2,11 @@ package visualizer
 
 import (
 	"BachelorThesis/engine/objects"
+	"log"
+	"math"
+	"runtime"
 
-	"github.com/g3n/engine/app"
-	"github.com/g3n/engine/camera"
-	"github.com/g3n/engine/core"
-	"github.com/g3n/engine/geometry"
-	"github.com/g3n/engine/graphic"
-	"github.com/g3n/engine/light"
-	"github.com/g3n/engine/material"
-	"github.com/g3n/engine/math32"
+	hg "github.com/harfang3d/harfang-go"
 )
 
 const (
@@ -18,37 +14,94 @@ const (
 )
 
 func Start(algorithm string, endChan chan bool, pool []objects.Object) {
-	a := app.App()
-	scene := core.NewNode()
+	hg.InputInit()
+	hg.WindowSystemInit()
 
-	cam := camera.New(1)
-	cam.SetPosition(0, 0, 3)
-	scene.Add(cam)
+	//подготовка окна для рисования
+	var resX int32 = 1280
+	var resY int32 = 720
+	win := hg.RenderInitWithWindowTitleWidthHeightResetFlags("Harfang Sample", resX, resY, hg.RFMSAA4X)
+	pipeline := hg.CreateForwardPipelineWithShadowMapResolution(4096)
 
-	light := light.NewDirectional(&math32.Color{1, 1, 1}, 1)
-	scene.Add(light)
+	//создание конвейера отрисовки
+	res := hg.NewPipelineResources()
+	vtxLayout := hg.VertexLayoutPosFloatNormUInt8()
+	sphereMdl := hg.CreateSphereModel(vtxLayout, 0.1, 8, 16)
+	sphereRef := res.AddModel("sphere", sphereMdl)
 
-	geom := geometry.NewBox(1, 1, 1)
-	mat := material.NewStandard(&math32.Color{1, 0, 0})
-	mesh := graphic.NewMesh(geom, mat)
-	scene.Add(mesh)
+	//будем использовать предкомпилированные шейдеры
+	shader := hg.LoadPipelineProgramRefFromFile("resources_compiled/core/shader/default.hps", res, hg.GetForwardPipelineInfo())
+	//определение материала (для цвета задается RGB от 0 до 1)
+	sphereMat := hg.CreateMaterialWithValueName0Value0ValueName1Value1(shader, "uDiffuseColor", hg.NewVec4WithXYZ(1, 0, 0), "uSpecularColor", hg.NewVec4WithXYZ(1, 0.8, 0))
 
-	a.Run(scene)
+	//создания.и конфигурация фонового цвета
+	scene := hg.NewScene()
+	scene.GetCanvas().SetColor(hg.NewColorWithRGB(0.1, 0.1, 0.1))
+	scene.GetEnvironment().SetAmbient(hg.NewColorWithRGB(0.1, 0.1, 0.1))
 
-	/*for {
+	//установка камеры
+	cam := hg.CreateCamera(scene, hg.TransformationMat4(hg.NewVec3WithXYZ(15.5, 5, -6), hg.NewVec3WithXYZ(0.4, -1.2, 0)), 0.01, 100)
+	scene.SetCurrentCamera(cam)
+
+	//добавление света
+	hg.CreateSpotLightWithDiffuseDiffuseIntensitySpecularSpecularIntensityPriorityShadowTypeShadowBias(scene, hg.TransformationMat4(hg.NewVec3WithXYZ(-8.8, 21.7, -8.8), hg.Deg3(60, 45, 0)), 0, hg.Deg(5), hg.Deg(30), hg.ColorGetWhite(), 1, hg.ColorGetWhite(), 1, 0, hg.LSTMap, 0.000005)
+
+	//создаем объекты сцены (сферы)
+	rows := [][]*hg.Transform{}
+	for z := float32(-100.0); z < 100.0; z += 2.0 {
+		row := []*hg.Transform{}
+		for x := float32(-100.0); x < 100.0; x += 2.0 {
+			node := hg.CreateObjectWithSliceOfMaterials(scene, hg.TranslationMat4(hg.NewVec3WithXYZ(x*0.1, 0.1, z*0.1)), sphereRef, hg.GoSliceOfMaterial{sphereMat})
+			row = append(row, node.GetTransform())
+		}
+		rows = append(rows, row)
+	}
+	angle := 0.0
+	rect := hg.NewIntRectWithSxSyExEy(0, 0, resX, resY)
+
+	log.Printf("Visualization objects created")
+
+	//основной цикл
+	for !hg.ReadKeyboard().Key(hg.KEscape) && hg.IsWindowOpen(win) {
+		dt := hg.TickClock()
+		angle += float64(hg.TimeToSecF(dt))
+
+		//поворот объектов сцены (узлов) на угол
+		for j, row := range rows {
+			rowY := math.Cos(angle + float64(j)*0.1)
+			for i, trs := range row {
+				/*pos := trs.GetPos()
+				pos.SetY(float32(0.1 * (rowY*math.Sin(angle+float64(i)*0.1)*6 + 6.5)))
+				trs.SetPos(pos)
+				*/
+				p := hg.NewVec3()
+				p, _ = trs.GetPosRot()
+				p.SetY(float32(0.1 * (rowY*math.Sin(angle+float64(i)*0.1)*6 + 6.5)))
+				trs.SetPos(p)
+
+			}
+		}
+
+		//обновление сцены
+		scene.Update(dt)
+
+		viewID := uint16(0)
+		//отправка сцены в pipeline
+		hg.SubmitSceneToPipelineWithFovAxisIsHorizontal(&viewID, scene, rect, true, pipeline, res)
+
+		//обновление экрана из буфера
+		hg.Frame()
+		hg.UpdateWindow(win)
+		runtime.GC()
+
 		select {
 		case <-endChan:
 			return
-
 		default:
-			offset++
-			rl.BeginDrawing()
-			rl.ClearBackground(rl.RayWhite)
-
-			header = fmt.Sprintf("FPS: %v\ni=%v", rl.GetFPS(), offset)
-			rl.DrawText(header, int32(offset), 0, 20, rl.DarkGray)
-
-			rl.EndDrawing()
+			continue
 		}
-	}*/
+	}
+
+	hg.RenderShutdown()
+	hg.DestroyWindow(win)
 }
--- go.mod
+++ go.mod
@@ -2,14 +2,4 @@ module BachelorThesis
 
 go 1.23.0
 
-require (
-	github.com/ebitengine/gomobile v0.0.0-20250329061421-6d0a8e981e4c
-	github.com/g3n/engine v0.2.0
-)
-
-require (
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210410170116-ea3d685f79fb // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	golang.org/x/image v0.25.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-)
+require github.com/harfang3d/harfang-go v0.3.2
